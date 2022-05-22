f62df9415bda73638fc4e79d71c0922f794ca6b6
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-05-22 16:09:01 -0400

    Improve TempleOSRSService

--- src/main/java/com/templeosrs/util/TempleOSRSService.java
+++ src/main/java/com/templeosrs/util/TempleOSRSService.java
@@ -1,6 +1,5 @@
 package com.templeosrs.util;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import okhttp3.Call;
@@ -9,6 +8,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class TempleOSRSService
 {
@@ -20,42 +20,45 @@ public class TempleOSRSService
 
 	private static final String PLAYER_OVERVIEW = "player/view/overview_skilling_view.php?player=";
 
+	private static final String BOSSES = "&tracking=bosses";
+
+	private static final String DURATION = "&duration=";
+
 	private static final String CLAN_OVERVIEW = "api/group_info.php?id=";
 
 	private static final String CLAN_ACHIEVEMENTS = "api/group_achievements.php?id=";
 
 	private static final String CLAN_EDIT = "api/edit_group.php?";
 
-	private static final String BOSSES = "&tracking=bosses";
-
-	private static final String DURATION = "&duration=";
-
-	private static String getJsonFromURL(String urlString) throws IOException
-	{
-		OkHttpClient client = new OkHttpClient();
-		Request request = new Request.Builder()
-			.url(urlString)
-			.build();
-
-		Response response = client.newCall(request).execute();
-
-		if (response.body() != null)
-		{
-			return response.body().string();
-		}
-		return null;
-	}
-
 	public static CompletableFuture<TempleOSRSPlayer> fetchUserGainsAsync(String player, String duration) throws Exception
 	{
+		String playerSkillsOverviewJSON = null;
+		String playerBossingOverviewJSON = null;
+		OkHttpClient client = new OkHttpClient();
+
 		String playerSkillsOverviewURL = HOST + PLAYER_OVERVIEW + player + DURATION + duration;
+		Request playerSkillsRequest = new Request.Builder()
+			.url(playerSkillsOverviewURL)
+			.build();
+
+		Call playerSkillsCall = client.newCall(playerSkillsRequest);
+		ResponseBody playerSkillsResponse = playerSkillsCall.execute().body();
+		if(playerSkillsResponse != null)
+		{
+			playerSkillsOverviewJSON = playerSkillsResponse.string();
+		}
+
 		String playerBossingOverviewURL = HOST + PLAYER_OVERVIEW + player + BOSSES + DURATION + duration;
+		Request playerBossingRequest = new Request.Builder()
+			.url(playerBossingOverviewURL)
+			.build();
 
-		String playerSkillsOverviewJSON;
-		String playerBossingOverviewJSON;
-
-		playerSkillsOverviewJSON = getJsonFromURL(playerSkillsOverviewURL);
-		playerBossingOverviewJSON = getJsonFromURL(playerBossingOverviewURL);
+		Call playerBossingCall = client.newCall(playerBossingRequest);
+		ResponseBody playerBossingResponse = playerBossingCall.execute().body();
+		if(playerBossingResponse != null)
+		{
+			playerBossingOverviewJSON = playerBossingResponse.string();
+		}
 
 		CompletableFuture<TempleOSRSPlayer> future = new CompletableFuture<>();
 		future.complete(new TempleOSRSPlayer(playerSkillsOverviewJSON, playerBossingOverviewJSON));
@@ -64,17 +67,33 @@ public class TempleOSRSService
 
 	public static CompletableFuture<TempleOSRSClan> fetchClanAsync(String clanID) throws Exception
 	{
+		String clanOverviewJSON = null;
+		String clanAchievementsJSON = null;
+		OkHttpClient client = new OkHttpClient();
+
 		String clanOverviewURL = HOST + CLAN_OVERVIEW + clanID;
+		Request overviewRequest = new Request.Builder()
+			.url(clanOverviewURL)
+			.build();
+
+		Call overviewCall = client.newCall(overviewRequest);
+		ResponseBody overviewResponse = overviewCall.execute().body();
+		if(overviewResponse != null)
+		{
+			clanOverviewJSON = overviewResponse.string();
+		}
 
 		String clanAchievementsURL = HOST + CLAN_ACHIEVEMENTS + clanID;
+		Request achievementsRequest = new Request.Builder()
+			.url(clanAchievementsURL)
+			.build();
 
-		String clanOverviewJSON;
-
-		String clanAchievementsJSON;
-
-		clanOverviewJSON = getJsonFromURL(clanOverviewURL);
-
-		clanAchievementsJSON = getJsonFromURL(clanAchievementsURL);
+		Call achievementsCall = client.newCall(achievementsRequest);
+		ResponseBody achievementsResponse = achievementsCall.execute().body();
+		if(achievementsResponse != null)
+		{
+			clanAchievementsJSON = achievementsResponse.string();
+		}
 
 		CompletableFuture<TempleOSRSClan> future = new CompletableFuture<>();
 		future.complete(new TempleOSRSClan(clanOverviewJSON, clanAchievementsJSON));
