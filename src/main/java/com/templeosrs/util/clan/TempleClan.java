30a2234e868c915092af75e2b9781f5264bbfc09
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-07-02 16:12:22 -0400

    Member refactoring, add current-top ehp/ehb response objects

--- src/main/java/com/templeosrs/util/clan/TempleClan.java
+++ src/main/java/com/templeosrs/util/clan/TempleClan.java
@@ -9,15 +9,23 @@ public class TempleClan
 
 	public boolean error = false;
 
-	public TempleClanResponse clanOverview;
+	public TempleClanOverviewResponse clanOverview;
 
-	public TempleClanAchievementData clanAchievements;
+	public TempleClanAchievementResponse clanAchievements;
 
-	public TempleClan(String clanOverviewJSON, String clanAchievementsJSON)
+	public TempleClanCurrentTopResponse clanCurrentTopEhp;
+
+	public TempleClanCurrentTopResponse clanCurrentTopEhb;
+
+	public TempleClan(String clanOverviewJSON, String clanAchievementsJSON, String clanCurrentTopEhpJSON, String clanCurrentTopEhbJSON)
 	{
-		clanOverview = gson.fromJson(clanOverviewJSON, TempleClanResponse.class);
+		clanOverview = gson.fromJson(clanOverviewJSON, TempleClanOverviewResponse.class);
 
-		clanAchievements = gson.fromJson(clanAchievementsJSON, TempleClanAchievementData.class);
+		clanAchievements = gson.fromJson(clanAchievementsJSON, TempleClanAchievementResponse.class);
+
+		clanCurrentTopEhp = gson.fromJson(clanCurrentTopEhpJSON, TempleClanCurrentTopResponse.class);
+
+		clanCurrentTopEhb = gson.fromJson(clanCurrentTopEhbJSON, TempleClanCurrentTopResponse.class);
 
 		if (Objects.nonNull(clanOverview.error))
 		{
